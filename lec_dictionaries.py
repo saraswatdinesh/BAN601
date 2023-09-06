394b04d029f129c5460ae8ea1b821761784ac737
Dinesh Saraswat <saraswatdinesh89@gmail.com>
2023-09-05 18:40:43 -0700

    Dictionary Letcure Notes and commands

---
+++ lec_dictionaries.py
@@ -0,0 +1,9 @@
+# Certain execution commands for the dictionaries in python.
+purse = dict()
+purse['money'] = 51
+purse['candy'] = 'yes'
+print(purse)
+print(purse['money'])
+
+purse['money'] = purse['money'] + 10
+print(purse['money'])
